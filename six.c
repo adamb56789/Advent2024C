9815b04f17909af8f05363341641bf6b02569dd5
Adam <adamb56789@gmail.com>
2025-08-31 20:28:10 +0100

    6.2 graph version works

--- six.c
+++ six.c
@@ -8,10 +8,16 @@
 #include <stdint.h>
 #include <immintrin.h>
 
+#define u8 uint8_t
+#define u16 uint16_t
+
 #define N 130
+// Row length includes new line
+#define R (N+1)
 #define M N*(N+1)
 // Highest number of walls in one line
-#define W 14
+#define W 15
+#define EDGE_ARRAY_LENGTH 4000
 
 typedef enum {
     UP = 0,
@@ -20,7 +26,34 @@ typedef enum {
     LEFT = 3
 } Direction;
 
-static const int steps[4] = {-(N + 1), 1, (N + 1), -1};
+static const int steps[4] = {-R, 1, R, -1};
+
+typedef struct {
+    u8 horizontal[N][W];
+    u8 vertical[N][W];
+} Walls;
+
+#define NONE 255
+
+typedef struct {
+    u8 x;
+    u8 y;
+    u8 direction;
+} Point;
+
+const Point NO_POINT = {NONE, NONE, NONE};
+
+typedef struct {
+    Point point;
+    u16 nextIndex;
+} Edge;
+
+#define EDGE_EXITS_LAB 0
+
+typedef struct {
+    Edge edges[EDGE_ARRAY_LENGTH];
+    u16 gridToEdge[N][N][4];
+} Graph;
 
 static int find_next(const char *ptr) {
     const __m256i needle = _mm256_set1_epi8('^'); // Sets 32 '^' in a row
@@ -36,7 +69,7 @@ static int find_next(const char *ptr) {
     }
 }
 
-static int countSetBytes(const uint8_t visited[]) {
+static int countSetBytes(const u8 visited[]) {
     int count = 0;
     for (int i = 0; i < M; i += 32) {
         const __m256i block = _mm256_load_si256((__m256i *) (visited + i)); // Loads 32 chars from the array
@@ -47,12 +80,12 @@ static int countSetBytes(const uint8_t visited[]) {
 }
 
 int countPointsVisitedByGuard(const char *ptr, const char *end) {
-    uint8_t visited[M + 32] = {0};
+    u8 visited[M + 32] = {0};
 
     const int start = find_next(ptr);
     visited[start] = 0xFF;
 
-    uint8_t direction = UP; // unsigned makes it do mod 4 with only add+and
+    u8 direction = UP; // unsigned makes it do mod 4 with only add+and
     int pos = start;
     int step = steps[direction];
     while (1) {
@@ -76,114 +109,185 @@ int countPointsVisitedByGuard(const char *ptr, const char *end) {
     return countSetBytes(visited);
 }
 
-static int findNextWall(const int horizontalWalls[N][W], const int verticalWalls[N][W], const int y, const int x,
-                        const uint8_t direction) {
-    if (direction == UP) {
-        int result = -1;
-        for (int i = 1; i < W; i++) {
-            if (verticalWalls[x][i] > y) {
-                result = verticalWalls[x][i-1];
-                break;
-            }
+static int pointOutsideLab(const Point point) {
+    return point.direction == NO_POINT.direction;
+}
+
+static u8 insertIndex(const u8 *arr, u8 n) {
+    u8 i = 0;
+    for (; i < W; ++i) {
+        if (arr[i] == 255 || n <= arr[i]) {
+            break;
         }
+    }
+    return i;
+}
+
+// ReSharper disable CppDFANotInitializedField
+static Point wallsNextPoint(const Walls *walls, const Point point) {
+    if (point.direction == LEFT) {
+        const u8 i = insertIndex(walls->vertical[point.x], point.y);
+        if (i == 0) return NO_POINT;
+        const Point result = {point.x, walls->vertical[point.x][i - 1] + 1, UP};
         return result;
     }
-    if (direction == DOWN) {
-        int result = -1;
-        for (int i = 0; i < W; i++) {
-            if (verticalWalls[x][i] > y) {
-                result = verticalWalls[x][i];
-                break;
-            }
-        }
-        if (result >= N) return -1;
+    if (point.direction == RIGHT) {
+        const u8 i = insertIndex(walls->vertical[point.x], point.y);
+        if (walls->vertical[point.x][i] == 255) return NO_POINT;
+        const Point result = {point.x, walls->vertical[point.x][i] - 1, DOWN};
         return result;
     }
-    if (direction == LEFT) {
-        int result = -1;
-        for (int i = 1; i < W; i++) {
-            if (horizontalWalls[x][i] > y) {
-                result = horizontalWalls[x][i-1];
-                break;
-            }
-        }
+    if (point.direction == DOWN) {
+        const u8 i = insertIndex(walls->horizontal[point.y], point.x);
+        if (i == 0) return NO_POINT;
+        const Point result = {walls->horizontal[point.y][i - 1] + 1, point.y, LEFT};
         return result;
     }
-    if (direction == RIGHT) {
-        int result = -1;
-        for (int i = 0; i < W; i++) {
-            if (horizontalWalls[x][i] > y) {
-                result = horizontalWalls[x][i];
-                break;
-            }
-        }
-        if (result >= N) return -1;
+    if (point.direction == UP) {
+        const u8 i = insertIndex(walls->horizontal[point.y], point.x);
+        if (walls->horizontal[point.y][i] == 255) return NO_POINT;
+        const Point result = {walls->horizontal[point.y][i] - 1, point.y, RIGHT};
         return result;
     }
+    printf("oh no\n");
     exit(-1);
 }
 
-static int isLoop(char grid[M], const int start, const int obstacle, uint8_t direction) {
-    grid[obstacle] = '#'; // Instead of copying the whole thing, set and reset.
-
-    uint8_t visited[M][4] = {0};
-    int pos = start;
-    int step = steps[direction];
-    while (1) {
-        if (visited[pos][direction]) {
-            grid[obstacle] = '.';
-            return 1;
-        }
-        visited[pos][direction] = 1;
-        const int next = pos + step;
-
-        if ((unsigned) next >= M) break;
-
-        const char c = grid[next];
-        if (c == '\n') {
-            break;
-        }
-        if (c != '#') {
-            pos = next;
-        } else {
-            direction = (direction + 1) % 4;
-            step = steps[direction];
-        }
-    }
-    grid[obstacle] = '.';
-    return 0;
+static void insertIntoSortedArray(u8 arr[W], const u8 val) {
+    int i = 0;
+    while (i < W && arr[i] < val) ++i;
+    memmove(&arr[i + 1], &arr[i], (W - 1 - i) * sizeof(u8));
+    arr[i] = val;
 }
 
+static void removeFromSortedArray(u8 arr[W], const u8 val) {
+    int i = 0;
+    while (i < W && arr[i] != val) ++i;
+    memmove(&arr[i], &arr[i + 1], (W - 1 - i) * sizeof(u8));
+}
+
+static int isLoop(const Graph *graph, const Walls *walls, const int start, u8 direction, const int obstacle) {
+    const u8 obstacleX = obstacle % R;
+    const u8 obstacleY = obstacle / R;
+    insertIntoSortedArray(walls->horizontal[obstacleY], obstacleX);
+    insertIntoSortedArray(walls->vertical[obstacleX], obstacleY);
+
+    u8 visited[EDGE_ARRAY_LENGTH] = {0};
+    u8 foundLoop = 0;
+
+    int x = start % R;
+    int y = start / R;
+    Point p = {x, y, (direction + 3) % 4};
+
+    Edge edge = {p, 0}; // Dummy value
+    while (1) {
+        p = edge.point;
+        u16 nextIndex;
+        if (
+            p.x == obstacleX && (p.direction == LEFT && p.y > obstacleY || p.direction == RIGHT && p.y < obstacleY) ||
+            p.y == obstacleY && (p.direction == DOWN && p.x > obstacleX || p.direction == UP && p.x < obstacleX)
+        ) {
+            p = wallsNextPoint(walls, p);
+            if (pointOutsideLab(p)) break;
+            if (abs(p.x - obstacleX) + abs(p.y - obstacleY)) {
+                // We've hit the obstacle, so need to do another wall navigation since it isn't in the graph
+                p = wallsNextPoint(walls, p);
+                if (pointOutsideLab(p)) break;
+            }
+            nextIndex = graph->gridToEdge[p.y][p.x][p.direction];
+            if (nextIndex == EDGE_EXITS_LAB) break;
+            edge = graph->edges[nextIndex];
+        } else {
+            nextIndex = edge.nextIndex;
+            edge = graph->edges[nextIndex];
+            if (nextIndex == EDGE_EXITS_LAB) break;
+        }
+        if (visited[nextIndex]) {
+            foundLoop = 1;
+            break;
+        }
+        visited[nextIndex] = 1;
+    }
+
+    removeFromSortedArray(walls->horizontal[obstacleY], obstacleX);
+    removeFromSortedArray(walls->vertical[obstacleX], obstacleY);
+    return foundLoop;
+}
 
 int countSuccessfulObstructionPositions(const char *ptr, const char *end) {
-    int horizontalWalls[N][W];
-    int verticalWalls[N][W];
+    Walls walls;
     for (int i = 0; i < N; ++i) {
-        int count = 0;
+        int countH = 0;
+        int countV = 0;
         for (int j = 0; j < N; ++j) {
-            if (ptr[i * (N + 1) + j] == '#') {
-                horizontalWalls[i][count++] = j;
+            if (ptr[i * R + j] == '#') {
+                walls.horizontal[i][countH++] = j;
+            }
+
+            if (ptr[j * R + i] == '#') {
+                walls.vertical[i][countV++] = j;
             }
         }
-        while (count < W) horizontalWalls[i][count++] = 999;
+        walls.horizontal[i][countH] = 255;
+        walls.vertical[i][countV] = 255;
     }
-    for (int j = 0; j < N; ++j) {
-        int count = 0;
-        for (int i = 0; i < N; ++i) {
-            if (ptr[i * (N + 1) + j] == '#') {
-                verticalWalls[j][count++] = i;
+
+    Graph graph = {0};
+
+    int edgeIndex = 1; // 0 is null
+    for (int y = 0; y < N; ++y) {
+        for (int x = 0; x < N; ++x) {
+            if (ptr[y * R + x] != '#') continue;
+            if (y < N - 1) {
+                const Point in = {x, y + 1, UP};
+                const Point out = wallsNextPoint(&walls, in);
+                if (!pointOutsideLab(out)) {
+                    graph.gridToEdge[in.y][in.x][in.direction] = edgeIndex;
+                    const Edge e = {out, EDGE_EXITS_LAB};
+                    graph.edges[edgeIndex++] = e;
+                }
+            }
+            if (x > 0) {
+                const Point in = {x - 1, y, RIGHT};
+                const Point out = wallsNextPoint(&walls, in);
+                if (!pointOutsideLab(out)) {
+                    graph.gridToEdge[in.y][in.x][in.direction] = edgeIndex;
+                    const Edge e = {out, EDGE_EXITS_LAB};
+                    graph.edges[edgeIndex++] = e;
+                }
+            }
+            if (y > 0) {
+                const Point in = {x, y - 1, DOWN};
+                const Point out = wallsNextPoint(&walls, in);
+                if (!pointOutsideLab(out)) {
+                    graph.gridToEdge[in.y][in.x][in.direction] = edgeIndex;
+                    const Edge e = {out, EDGE_EXITS_LAB};
+                    graph.edges[edgeIndex++] = e;
+                }
+            }
+            if (x < N - 1) {
+                const Point in = {x + 1, y, LEFT};
+                const Point out = wallsNextPoint(&walls, in);
+                if (!pointOutsideLab(out)) {
+                    graph.gridToEdge[in.y][in.x][in.direction] = edgeIndex;
+                    const Edge e = {out, EDGE_EXITS_LAB};
+                    graph.edges[edgeIndex++] = e;
+                }
             }
         }
-        while (count < W) verticalWalls[j][count++] = 999;
+    }
+
+    // We can't add the nextIndex on first pass since we don't know if there is a next edge or where it is
+    for (int i = 1; i < edgeIndex; ++i) {
+        const Point p = graph.edges[i].point;
+        graph.edges[i].nextIndex = graph.gridToEdge[p.y][p.x][p.direction];
     }
 
     int count = 0;
-    char grid[M];
-    memcpy(grid, ptr, M);
-    uint8_t visited[M + 32] = {0};
+    u8 visited[M] = {0};
 
-    const int start = find_next(grid);
-    uint8_t direction = UP;
+    const int start = find_next(ptr);
+    u8 direction = UP;
     visited[start] = 1;
 
     int pos = start;
@@ -193,13 +297,13 @@ int countSuccessfulObstructionPositions(const char *ptr, const char *end) {
 
         if ((unsigned) next >= M) break;
 
-        const char c = grid[next];
+        const char c = ptr[next];
         if (c == '\n') {
             break;
         }
         if (c != '#') {
             if (!visited[next]) {
-                count += isLoop(grid, pos, next, direction);
+                count += isLoop(&graph, &walls, pos, direction, next);
             }
             visited[next] = 1;
             pos = next;
@@ -210,6 +314,7 @@ int countSuccessfulObstructionPositions(const char *ptr, const char *end) {
     }
 
     return count;
+    return walls.vertical[129][0] == 123 || walls.horizontal[129][0] == 123? 0 : 1516;
 }
 
 // 0.004070 ms
@@ -219,7 +324,7 @@ void six_1() {
     benchmarkFunctionOnFile("../input/6.txt", &countPointsVisitedByGuard, 400000, 4433);
 }
 
-// 10.63 ms
+// 0.915 ms
 void six_2() {
-    benchmarkFunctionOnFile("../input/6.txt", &countSuccessfulObstructionPositions, 100, 1516);
+    benchmarkFunctionOnFile("../input/6.txt", &countSuccessfulObstructionPositions, 1000, 1516);
 }
